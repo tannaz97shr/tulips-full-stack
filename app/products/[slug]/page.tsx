bf73d1acd2431c4b04ae11a3757f355a16b9c521
Tannaz Shirzadi <tia.shr97@gmail.com>
2026-08-18 07:09:48 +1000

    style: apply consistent formatting to product detail page

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -1,8 +1,10 @@
-import { notFound } from "next/navigation";
 import { ProductDetailView } from "@/modules/catalog/components/ProductDetailView";
 import { PLACEHOLDER_PRODUCTS } from "@/modules/catalog/data/placeholder-products";
+import { notFound } from "next/navigation";
 
-export default async function ProductDetailPage(props: PageProps<"/products/[slug]">) {
+export default async function ProductDetailPage(
+  props: PageProps<"/products/[slug]">,
+) {
   const { slug } = await props.params;
   const product = PLACEHOLDER_PRODUCTS.find((item) => item.slug === slug);
 
@@ -11,9 +13,13 @@ export default async function ProductDetailPage(props: PageProps<"/products/[slu
   }
 
   const related = PLACEHOLDER_PRODUCTS.filter(
-    (item) => item.id !== product.id && item.category === product.category
+    (item) => item.id !== product.id && item.category === product.category,
   )
-    .concat(PLACEHOLDER_PRODUCTS.filter((item) => item.id !== product.id && item.category !== product.category))
+    .concat(
+      PLACEHOLDER_PRODUCTS.filter(
+        (item) => item.id !== product.id && item.category !== product.category,
+      ),
+    )
     .slice(0, 4);
 
   return <ProductDetailView product={product} related={related} />;
